fd301639195a46da37d6d5bbb8e343f991289d57
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-09-18 15:52:24 -0300

    Rotas de criação e listagem

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
+    const { method, url } = req
+
+    if(method == 'GET' && url == "/users") {
+        //Early Return
+        return res.end("Listagem de usuário")
+
+    }
+
+    if(method == "POST" && url == "/users") {
+        return res.end("Criação de usuário")
+    }
+ 
     return res.end("Hello World")
 })
 
