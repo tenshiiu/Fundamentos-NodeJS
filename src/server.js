6d3b90ba36b434e0219e075cff1bffd83cc3099b
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-09-18 16:16:48 -0300

    Conhecendo HTTP status codes

--- src/server.js
+++ src/server.js
@@ -20,10 +20,10 @@ const server = http.createServer((req, res) => {
             email: "johndoe@example.com"
         })
 
-        return res.end("Criação de usuário")
+        return res.writeHead(201).end()
     }
  
-    return res.end("Hello World")
+    return res.writeHead(404).end("Not Found")
 })
 
 server.listen(3333)
